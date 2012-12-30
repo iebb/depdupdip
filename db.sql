a2476d70a2d519cbb41745edc33de4266527d044
Jebwiz Oscar <jebwizoscar@hotmail.com>
2012-12-30 20:01:50 +0800

    Ver 0415 MODIFY INSTALL INFO
    MODIFY INSTALL INFO

---
+++ db.sql
@@ -0,0 +1,248 @@
+
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+CREATE TABLE IF NOT EXISTS `compileinfo` (
+  `solution_id` int(11) NOT NULL DEFAULT '0',
+  `error` text,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `contest` (
+  `contest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `description` text,
+  `private` tinyint(4) NOT NULL DEFAULT '0',
+  `langmask` int(11) NOT NULL DEFAULT '0' COMMENT 'bits for LANG to mask',
+  PRIMARY KEY (`contest_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1001 ;
+
+CREATE TABLE IF NOT EXISTS `contest_problem` (
+  `problem_id` int(11) NOT NULL DEFAULT '0',
+  `contest_id` int(11) DEFAULT NULL,
+  `title` char(200) NOT NULL DEFAULT '',
+  `num` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `crawl` (
+  `problem_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ojid` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `description` text,
+  `input` text,
+  `output` text,
+  `sample_input` text,
+  `sample_output` text,
+  `oj` varchar(20) DEFAULT NULL,
+  `hint` text,
+  `source` varchar(100) DEFAULT NULL,
+  `in_date` datetime DEFAULT NULL,
+  `time_limit` int(11) NOT NULL DEFAULT '0',
+  `memory_limit` int(11) NOT NULL DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `accepted` int(11) DEFAULT '0',
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  `submiturl` varchar(60) NOT NULL,
+  PRIMARY KEY (`problem_id`),
+  UNIQUE KEY `ojid` (`ojid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1996 ;
+
+CREATE TABLE IF NOT EXISTS `loginlog` (
+  `user_id` varchar(48) NOT NULL DEFAULT '',
+  `password` varchar(40) DEFAULT NULL,
+  `ip` varchar(100) DEFAULT NULL,
+  `time` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mail` (
+  `mail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `to_user` varchar(48) NOT NULL DEFAULT '',
+  `from_user` varchar(48) NOT NULL DEFAULT '',
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `content` text,
+  `new_mail` tinyint(1) NOT NULL DEFAULT '1',
+  `reply` tinyint(4) DEFAULT '0',
+  `in_date` datetime DEFAULT NULL,
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`mail_id`),
+  KEY `uid` (`to_user`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1023 ;
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `news_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(48) NOT NULL DEFAULT '',
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `importance` tinyint(4) NOT NULL DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`news_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1008 ;
+
+CREATE TABLE IF NOT EXISTS `online` (
+  `hash` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `ip` varchar(20) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `ua` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `refer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastmove` int(10) NOT NULL,
+  `firsttime` int(10) DEFAULT NULL,
+  `uri` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`hash`),
+  UNIQUE KEY `hash` (`hash`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `preferences` (
+  `user_id` varchar(40) NOT NULL,
+  `ppp` int(3) NOT NULL DEFAULT '40',
+  `customhtml` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `privilege` (
+  `user_id` char(48) NOT NULL DEFAULT '',
+  `rightstr` char(30) NOT NULL DEFAULT '',
+  `defunct` char(1) NOT NULL DEFAULT 'N'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `problem` (
+  `problem_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `description` text,
+  `input` text,
+  `output` text,
+  `sample_input` text,
+  `sample_output` text,
+  `spj` char(1) NOT NULL DEFAULT '0',
+  `hint` text,
+  `source` varchar(100) DEFAULT NULL,
+  `in_date` datetime DEFAULT NULL,
+  `time_limit` int(11) NOT NULL DEFAULT '0',
+  `memory_limit` int(11) NOT NULL DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `accepted` int(11) DEFAULT '0',
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  `poj_id` int(11) NOT NULL DEFAULT '-1',
+  PRIMARY KEY (`problem_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=308 ;
+
+CREATE TABLE IF NOT EXISTS `reply` (
+  `rid` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` varchar(48) NOT NULL,
+  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `content` text NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `ip` varchar(30) NOT NULL,
+  PRIMARY KEY (`rid`),
+  KEY `author_id` (`author_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `result` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `solution_id` int(11) NOT NULL,
+  `file` varchar(40) NOT NULL,
+  `time` int(11) NOT NULL,
+  `memory` int(11) NOT NULL,
+  `info` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6274 ;
+
+CREATE TABLE IF NOT EXISTS `runtimeinfo` (
+  `solution_id` int(11) NOT NULL DEFAULT '0',
+  `error` text,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `sim` (
+  `s_id` int(11) NOT NULL,
+  `sim_s_id` int(11) DEFAULT NULL,
+  `sim` int(11) DEFAULT NULL,
+  PRIMARY KEY (`s_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `solution` (
+  `solution_id` int(11) NOT NULL AUTO_INCREMENT,
+  `problem_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` char(48) NOT NULL,
+  `time` int(11) NOT NULL DEFAULT '0',
+  `memory` int(11) NOT NULL DEFAULT '0',
+  `in_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `result` smallint(6) NOT NULL DEFAULT '0',
+  `language` int(10) unsigned NOT NULL DEFAULT '0',
+  `ip` char(15) NOT NULL,
+  `contest_id` int(11) DEFAULT NULL,
+  `valid` tinyint(4) NOT NULL DEFAULT '1',
+  `num` tinyint(4) NOT NULL DEFAULT '-1',
+  `code_length` int(11) NOT NULL DEFAULT '0',
+  `judgetime` datetime DEFAULT NULL,
+  `pass_rate` decimal(2,2) unsigned NOT NULL DEFAULT '0.00',
+  `judger` char(20) NOT NULL DEFAULT 'Zero',
+  `jcolor` varchar(10) NOT NULL DEFAULT 'info',
+  PRIMARY KEY (`solution_id`),
+  KEY `uid` (`user_id`),
+  KEY `pid` (`problem_id`),
+  KEY `res` (`result`),
+  KEY `cid` (`contest_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1571 ;
+
+CREATE TABLE IF NOT EXISTS `source_code` (
+  `solution_id` int(11) NOT NULL,
+  `source` text NOT NULL,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `templates` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `topic` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varbinary(60) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `top_level` int(2) NOT NULL DEFAULT '0',
+  `cid` int(11) DEFAULT NULL,
+  `pid` int(11) NOT NULL,
+  `author_id` varchar(48) NOT NULL,
+  PRIMARY KEY (`tid`),
+  KEY `cid` (`cid`,`pid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` varchar(48) NOT NULL DEFAULT '',
+  `email` varchar(100) DEFAULT NULL,
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `ip` varchar(20) NOT NULL DEFAULT '',
+  `accesstime` datetime DEFAULT NULL,
+  `volume` int(11) NOT NULL DEFAULT '1',
+  `language` int(11) NOT NULL DEFAULT '1',
+  `password` varchar(32) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  `nick` varchar(100) NOT NULL DEFAULT '',
+  `school` varchar(100) NOT NULL DEFAULT '',
+  `color` varchar(6) NOT NULL DEFAULT '0088CC',
+  `qq` varchar(40) NOT NULL DEFAULT '!',
+  `wb` varchar(50) NOT NULL DEFAULT '!',
+  `rr` varchar(40) NOT NULL DEFAULT '!',
+  `i1_uid` varchar(20) NOT NULL DEFAULT '!',
+  `i1_pwd` varchar(20) NOT NULL,
+  `template` varchar(20) NOT NULL DEFAULT 'classic',
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
